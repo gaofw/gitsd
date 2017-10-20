a75354c649bb2cc6dde3ec3b16e02462402481fd
gaofangwei <fangweigao@credirease.cn>
2017-10-20 11:21:32 +0800

    测试提价

---
+++ src/main/java/com/gfw/Demo.java
@@ -0,0 +1,25 @@
+package main.java.com.gfw;
+
+public class Demo {
+
+    private String test = "";
+
+    private int x = 1;
+
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
---
+++ src/main/java/com/gfw/Test.java
@@ -0,0 +1,8 @@
+package main.java.com.gfw;
+
+public class Test extends  Demo{
+
+    public void aa(){
+        Demo demo = new Demo();
+    }
+}
