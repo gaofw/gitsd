bd5f4c03296289ca8c389e1ea69ce95b9b26dd9c
gaofangwei <fangweigao@credirease.cn>
2017-10-20 18:40:58 +0800

    Spring Boot demo

---
+++ src/main/java/com/gfw/App.java
@@ -0,0 +1,24 @@
+package com.gfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.setUriEncoding("UTF-8");
+        tomcat.setPort(8090);
+        return tomcat;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ src/main/java/com/gfw/rest/IsAliveController.java
@@ -0,0 +1,13 @@
+package com.gfw.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IsAliveController {
+
+    @RequestMapping("isAlive")
+    public String isAlive(){
+        return "1";
+    }
+}
